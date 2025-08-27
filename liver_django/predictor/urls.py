2dbba39dfd96217c911a62347c4e43bccd403646
The_mRu <fsrrayhan@gmail.com>
2025-08-28 02:51:09 +0600

    emnee e

--- liver_django/predictor/urls.py
+++ liver_django/predictor/urls.py
@@ -1,12 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.home, name='home'),
-#     # path('api/predict/', views.api_predict, name='api_predict'),
-#     path('predict/', views.predict_page, name='predict_page'),
-# ]
-
 from django.urls import path
 from . import views
 
